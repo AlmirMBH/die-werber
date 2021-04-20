4bbd327a903b50f3845b6e68578417bb9bda0e79
AlmirMBH <almir.mustafic.tz@gmail.com>
2021-04-20 15:41:26 +0200

    custom colors added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            titleBlack: {
+                light: '#181818',
+                DEFAULT: '#191919',
+                dark: '#202020',
+            },
+            titleRed: {
+                light: '#F36377',
+                DEFAULT: '#F25266',
+                dark: '#F14155',
+            }
+        },
         extend: {},
         fontFamily: {
             'nunito': ['nunito', 'sans-serif'],
