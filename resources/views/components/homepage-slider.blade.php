96df987cf48dbeae060cb81b545146875fc5bcbb
Zekerijah <zekerijahsehic1@gmail.com>
2021-05-28 14:43:14 +0200

    hompepage slider v3

--- resources/views/components/homepage-slider.blade.php
+++ resources/views/components/homepage-slider.blade.php
@@ -20,16 +20,16 @@
 			<link href="https://fonts.googleapis.com/css?family=Nunito:400%2C700%7CRoboto:400" rel="stylesheet" property="stylesheet" media="all" type="text/css" >
 
 
-			<link rel="stylesheet" type="text/css" href="fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
-			<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+			<link rel="stylesheet" type="text/css" href="{{ asset('fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css') }}">
+			<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome/css/font-awesome.css') }}">
 
 			<!-- REVOLUTION STYLE SHEETS -->
-			<link rel="stylesheet" type="text/css" href="css/rs6.css">
+			<link rel="stylesheet" type="text/css" href="{{ asset('css/rs6.css') }}">
 			<!-- REVOLUTION LAYERS STYLES -->
 							<style type="text/css"></style>
 			<!-- REVOLUTION JS FILES -->
-			<script type="text/javascript" src="js/revolution.tools.min.js"></script>
-			<script type="text/javascript" src="js/rs6.min.js"></script>
+			<script type="text/javascript" src="{{ asset('js/revolution.tools.min.js') }}"></script>
+			<script type="text/javascript" src="{{ asset('js/rs6.min.js') }}"></script>
 
 			<script type="text/javascript">function setREVStartSize(t){try{var h,e=document.getElementById(t.c).parentNode.offsetWidth;if(e=0===e||isNaN(e)?window.innerWidth:e,t.tabw=void 0===t.tabw?0:parseInt(t.tabw),t.thumbw=void 0===t.thumbw?0:parseInt(t.thumbw),t.tabh=void 0===t.tabh?0:parseInt(t.tabh),t.thumbh=void 0===t.thumbh?0:parseInt(t.thumbh),t.tabhide=void 0===t.tabhide?0:parseInt(t.tabhide),t.thumbhide=void 0===t.thumbhide?0:parseInt(t.thumbhide),t.mh=void 0===t.mh||""==t.mh||"auto"===t.mh?0:parseInt(t.mh,0),"fullscreen"===t.layout||"fullscreen"===t.l)h=Math.max(t.mh,window.innerHeight);else{for(var i in t.gw=Array.isArray(t.gw)?t.gw:[t.gw],t.rl)void 0!==t.gw[i]&&0!==t.gw[i]||(t.gw[i]=t.gw[i-1]);for(var i in t.gh=void 0===t.el||""===t.el||Array.isArray(t.el)&&0==t.el.length?t.gh:t.el,t.gh=Array.isArray(t.gh)?t.gh:[t.gh],t.rl)void 0!==t.gh[i]&&0!==t.gh[i]||(t.gh[i]=t.gh[i-1]);var r,a=new Array(t.rl.length),n=0;for(var i in t.tabw=t.tabhide>=e?0:t.tabw,t.thumbw=t.thumbhide>=e?0:t.thumbw,t.tabh=t.tabhide>=e?0:t.tabh,t.thumbh=t.thumbhide>=e?0:t.thumbh,t.rl)a[i]=t.rl[i]<window.innerWidth?0:t.rl[i];for(var i in r=a[0],a)r>a[i]&&0<a[i]&&(r=a[i],n=i);var d=e>t.gw[n]+t.tabw+t.thumbw?1:(e-(t.tabw+t.thumbw))/t.gw[n];h=t.gh[n]*d+(t.tabh+t.thumbh)}void 0===window.rs_init_css&&(window.rs_init_css=document.head.appendChild(document.createElement("style"))),document.getElementById(t.c).height=h,window.rs_init_css.innerHTML+="#"+t.c+"_wrapper { height: "+h+"px }"}catch(t){console.log("Failure at Presize of Slider:"+t)}};</script>
 		</head>
